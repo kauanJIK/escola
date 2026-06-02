10891f847b22559af2af5645c6b9b2b89dbeda9d
unknown <20241pvai0030017@estudantes.ifpr.edu.br>
2026-06-02 09:32:27 -0300

    codido 2.0

--- introducao_arc.py
+++ introducao_arc.py
@@ -8,13 +8,32 @@ TITULO = "Meu Joguinho"
 class Player(arcade.Sprite):
 
     def __init__(self):
-        super().__init__("Luffy.png",scale=0.1)
-        self.textura_direita = arcade.load_texture("Luffy.png")
+        super().__init__("luffy.png",scale=0.3)
+        self.textura_direita = arcade.load_texture("luffy.png")
         self.textura_esquerda = arcade.load_texture("Luffy2.png")
         
 
     def update(self,delta_time):
-       pass
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+
+        if self.center_x > 0:
+           self.textura_direita = self.textura_direita
+        elif self.center_x < 0:
+            self.textura_esquerda = self.textura_esquerda
+    
+           
+
+class Moeda(arcade.Sprite):
+    def __init__(self):
+        super().__init__("moeda.png",scale=0.1)
+        self.textura_direita = arcade.load_texture("moeda.png")
+        
+
+    def update(self,delta_time):
+       self.center_x += self.change_x
+       self.center_y += self.change_y
 
 
 class MeuJogo(arcade.Window):
@@ -23,17 +42,27 @@ class MeuJogo(arcade.Window):
 
 
         arcade.set_background_color((226, 237, 5))
-        self.personagem = Player()
-        self.personagem.center_x = 400
-        self.personagem.center_y = 200
-        
+        self.jogador = Player()
+        self.jogador.center_x = 400
+        self.jogador.center_y = 320
+
+        self.sprite_jogador = arcade.SpriteList()
+        self.sprite_jogador.append(self.jogador)
+
+        self.moeda_jogo = Moeda()
+        self.moeda_jogo.center_x = 280
+        self.moeda_jogo.center_y = 100
+
+        self.sprite_moeda_jogo = arcade.SpriteList()
+        self.sprite_moeda_jogo.append(self.moeda_jogo)
 
     def on_draw(self):
         self.clear()
-        arcade.draw_sprite(self.personagem)
+        self.sprite_jogador.draw()
+        self.sprite_moeda_jogo.draw()
+        
 
-    def on_update(self, delta_time):
-        pass
+        
 
 
 
